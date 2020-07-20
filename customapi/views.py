e34824c0c764314f7be2162b95193e040aab02c3
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-07-20 11:56:58 -0700

    Delete and Put

--- customapi/views.py
+++ customapi/views.py
@@ -12,7 +12,8 @@ def homepage(request):
 	return render(request,'customapi/homepage.html')
 
 
-
+class NotFoundException (Exception):
+    pass
 
 class DetailsListView(APIView):
 
@@ -34,10 +35,39 @@ class DetailsListView(APIView):
 
 class DetailView(APIView):
 
-	def get(self, request, id):
+
+	def get_detail(self,id):
+		try:
+			detail = Details.objects.get(id = id)
+			return detail
+		except Exception as ex:
+			return ex
+			
+
+	def get(self,request,id):
 		detail = Details.objects.get(id = id)
 		serializer = DetailSerializer(detail, many=False)
 		serializer.data
 		return Response(serializer.data)
+
+
+	def put (self, request, id):
+		try:
+			detail = self.get_detail(id)
+			serializer = DetailSerializer(detail, data=request.data)
+			if serializer.is_valid():
+				serializer.save()
+				return Response(serializer.data, status=status.HTTP_200_OK)
+			return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+		except NotFoundException:
+			return Response(status=status.HTTP_404_NOT_FOUND)	
+
+	def delete (self, request, id):
+		try :
+			detail = self.get_detail(id)
+			detail.delete()
+			return Response(status=status.HTTP_200_OK)
+		except NotFoundException:
+			return Response(status=status.HTTP_404_NOT_FOUND)        
 
 
